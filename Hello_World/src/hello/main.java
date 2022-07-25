937b279ffb62034075f646ed86bef784fcf4511c
binn328 <binn328@gmail.com>
2022-07-25 18:59:21 +0900

    New file

---
+++ Hello_World/src/hello/main.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		System.out.println("한글 테스트입니다.");
+	}
+
+}
